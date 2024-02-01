a72fcbe5c1b5058dcac5d2c33d2fbacb869ca0b1
David <david.genfors@gmail.com>
2024-02-01 17:25:05 +0100

    stuff

--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
-const port = 3000;
+const port = 3001;
+
+app.use(cors()); // Enable CORS for all routes
 
 app.get('/', (req, res) => {
-  res.send('Hello, Express!');
+  res.send('Hello there!');
 });
 
 app.listen(port, () => {
