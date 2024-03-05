22e20d418d46824196c20df47afd2d1305a5ed28
dgenfors <David.genfors@gmail.com>
2024-03-05 17:00:25 +0100

    Update Unauthorized.js

--- api/apiReq/Unauthorized.js
+++ api/apiReq/Unauthorized.js
@@ -13,7 +13,7 @@ router.post('/login', async (req, res) => {
     if(!data){
       res.json({state: false});
     }else{
-      res.cookie('jwt', data.token, { httpOnly: true, secure: true, domain: 'iv1201-vt24-frontend' });
+      res.cookie('jwt', data.token, { httpOnly: true, secure: true, domain: '.vercel.app' });
       res.json({state : true, role_id: data.user.role});
     }
   } catch (error) {
